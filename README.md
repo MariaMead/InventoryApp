da3c1a225271db82a254d55c052c78bbd7246b90
MariaMead <mmead@Academic.RRC.CA>
2025-12-03 09:27:50 -0600

    fixed more linting problems

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # InventoryApp
 
-### Comp:3020 Cloud Infrastructure Project
+## Comp:3020 Cloud Infrastructure Project
 
 Lambda Functions for AWS Inventory Application and workflows.
--- lambda/delete_inventory_item/lambda_function.py
+++ lambda/delete_inventory_item/lambda_function.py
@@ -6,49 +6,49 @@ from boto3.dynamodb.conditions import Key
 
 """AWS Lambda Function for Inventory App"""
 def lambda_handler(event, context):
-    dynamodb = boto3.resource('dynamodb')
-    table_name = 'Inventory'
+    dynamodb = boto3.resource("dynamodb")
+    table_name = "Inventory"
 
     table = dynamodb.Table(table_name)
 
     # Extract the '_id' from the path parameters
-    if 'pathParameters' not in event or 'id' not in event['pathParameters']:
+    if "pathParameters" not in event or 'id' not in event["pathParameters"]:
         return {
-            'statusCode': 400,
-            'body': json.dumps("Missing 'id' path parameter")
+            "statusCode": 400,
+            "body": json.dumps("Missing 'id' path parameter")
         }
 
-    item_id = event['pathParameters']['id']
+    item_id = event["pathParameters"]["id"]
 
     # Attempt to delete the item from the table
     try:
         response = table.query(
-            KeyConditionExpression=Key('item_id').eq(item_id)
+            KeyConditionExpression=Key("item_id").eq(item_id)
         )
-        items = response.get('Items', [])
+        items = response.get("Items", [])
         
         if not items:
             return {
-                'statusCode': 404,
-                'body': json.dumps("Item not found")
+                "statusCode": 404,
+                "body": json.dumps("Item not found")
             }
         for item in items:
-            item_location_id = item['item_location_id']
+            item_location_id = item["item_location_id"]
             table.delete_item(
                 Key={
-                    'item_id': item_id,
-                    'item_location_id': item_location_id
+                    "item_id": item_id,
+                    "item_location_id": item_location_id
                 }
             )
 
         return {
-            'statusCode': 200,
-            'body': json.dumps(f"Item with ID {item_id} deleted successfully.")
+            "statusCode": 200,
+            "body": json.dumps(f"Item with ID {item_id} deleted successfully.")
         }
 
     except Exception as e:
         print(e)
         return {
-            'statusCode': 500,
-            'body': json.dumps(f"Error deleting item: {str(e)}")
+            "statusCode": 500,
+            "body": json.dumps(f"Error deleting item: {str(e)}")
         }
